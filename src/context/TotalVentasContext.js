618b4f287ea86a9c5184268f64940a9057b2162b
CristianDB1 <150852071+CristianDB1@users.noreply.github.com>
2025-08-20 15:54:51 -0500

    feat(frontend) crear contexto global
    creamos un contexto global que almacena el total acumulado de las ventas

---
+++ src/context/TotalVentasContext.js
@@ -0,0 +1,29 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const TotalVentaContext = createContext(); //Creamos el contexto de esta manera
+
+//Aqui se crea un provider que es el que va a envolver la app
+export const TotalVentaProvider = ({children}) => {  
+    const [total, setTotal] = useState(0);
+
+    //Creamos funciones de utilidad
+    const addToTotal = (monto) => {
+        setTotal((prev) => prev + monto);
+    };
+
+    const resetTotal = () => {
+        setTotal(0);
+    };
+
+    return (
+        <TotalVentaContext.Provider value={{total,addToTotal,resetTotal}}>
+            {children}
+        </TotalVentaContext.Provider>
+    );
+};
+
+//Creamos un hook personalizado para usar el contecto mas facil
+export const useTotalVenta = () => {
+    return useContext(Total)
+}
