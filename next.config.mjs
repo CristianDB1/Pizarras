676ea7f526aac8942a9a4901ec9ac0482c21c5f6
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-09-01 16:15:58 -0500

    fix cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   async headers() {
     return [
       {
@@ -16,4 +13,5 @@ const nextConfig = {
     ];
   },
 }
-export default nextConfig;
+
+export default nextConfig
