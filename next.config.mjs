b31649c79c12d8def5e4ea1ef050ebdf84ec5223
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-09-01 16:19:49 -0500

    fix cambios de despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,30 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { 
+            key: 'Access-Control-Allow-Origin', 
+            value: '*' 
+          },
+          { 
+            key: 'Access-Control-Allow-Methods', 
+            value: 'GET, POST, PUT, DELETE, OPTIONS' 
+          },
+          { 
+            key: 'Access-Control-Allow-Headers', 
+            value: 'Content-Type, Authorization' 
+          },
         ],
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  }
 }
 
 export default nextConfig
