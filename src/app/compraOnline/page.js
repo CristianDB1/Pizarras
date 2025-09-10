5cff8fa16705ad130b1b776cc21e4c6af6b98cb0
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-09-10 10:36:44 -0500

    feat creacion de page.js de compra online
    Se creo el page.js

---
+++ src/app/compraOnline/page.js
@@ -0,0 +1,11 @@
+import ComprarOnline from "@/components/custom/boletosOnline/ComprarOnline";
+
+const ComprarOnlinePage = () => {
+  return (
+    <div className="w-full h-screen bg-[rgb(38,38,38)]">
+      <ComprarOnline />
+    </div>
+  );
+};
+
+export default ComprarOnlinePage;
