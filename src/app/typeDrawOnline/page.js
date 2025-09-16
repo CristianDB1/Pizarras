baaaf486f24e3cafd86004c23bb3ead2af3d2a1d
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-09-16 10:39:44 -0500

    feat bienvenida e ingreso a tipo de boleto
    Se coloca el inicio como una bienvenida antes de entrar a elegir que tipo de boleto es el que va a tomar

---
+++ src/app/typeDrawOnline/page.js
@@ -0,0 +1,11 @@
+'use client'
+import TypeDrawOnline from "@/components/custom/boletosOnline/TypeDrawOnline";
+
+export default function TypeDrawPage() {
+    return(
+        <div className="w-full min-h-screen bg-[rgb(38,38,38)] flex items-center justify-center p-6">
+            <TypeDrawOnline/>
+        </div>
+    );
+
+}
--- src/app/welcomeOnline/page.js
+++ src/app/welcomeOnline/page.js
@@ -1,9 +1,10 @@
-import TypeDrawOnline from "@/components/custom/boletosOnline/TypeDrawOnline";
+'use client'
+import WelcomeBoletosOnline from "@/components/custom/boletosOnline/WelcomeBoletosOnline";
 
 export default function BoletosOnlinePage() {
   return (
     <div className="w-full min-h-screen bg-[rgb(38,38,38)] flex items-center justify-center p-6">
-      <TypeDrawOnline />
+      < WelcomeBoletosOnline />
     </div>
   );
 }
