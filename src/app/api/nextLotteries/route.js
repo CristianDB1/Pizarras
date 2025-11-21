4ae25d1ab517c0edf92577f471413b06fbf694c7
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-11-21 16:42:47 -0500

    blanco

--- src/app/api/nextLotteries/route.js
+++ src/app/api/nextLotteries/route.js
@@ -10,7 +10,7 @@ export async function GET(req) {
     const [rows] = await pool.query(`
       SELECT * FROM sorteo
       WHERE Estatus = 'abierto'
-      AND Tipo_sorteo != 'especial'  -- o el campo que uses para identificar
+      AND Tipo_sorteo != 'especial'
       ORDER BY Fecha ASC
       LIMIT 3
     `);
