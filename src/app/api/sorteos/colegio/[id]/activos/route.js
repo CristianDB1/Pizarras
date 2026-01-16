c84bcbc1f37a84c97f71d24d3db3b2f99996ca83
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2026-01-16 11:08:44 -0500

    fix

--- src/app/api/sorteos/colegio/[id]/activos/route.js
+++ src/app/api/sorteos/colegio/[id]/activos/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import pool from "@/db/MysqlConection";
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export async function GET(request, { params }) {
     try {
